3b4cdfd4048cf42e62189aea720b81acc8b31884
Raviteja Y S <85568564+ravitejays@users.noreply.github.com>
2021-12-08 20:23:55 +0530

    Add files via upload

---
+++ 1stcode.py
@@ -0,0 +1,3 @@
+r = float(input('Please enter the Radius of the circle:'))
+Area = 3.142*r**2
+print("The area of the Circle is ", Area)
---
+++ 2ndcode.py
@@ -0,0 +1,3 @@
+filename = str(input(print('Enter the filename:')))
+file = filename.split(".")
+print("Extension of the file you entered is: " + file[-1])
